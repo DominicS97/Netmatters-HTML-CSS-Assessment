276a37508261a1c51b5ceb952221ab50b660a4a2
DominicS97 <dejfsutcliffe@gmail.com>
2024-06-19 11:24:31 +0100

    banner working

---
+++ script.js
@@ -0,0 +1,223 @@
+const banner_1 = document.getElementById("banner-1");
+const banner_1b = document.getElementById("banner-1b");
+const banner_1span = document.getElementById("icon-banner-1");
+
+// Add a mouseover event listener
+banner_1.addEventListener("mouseover", () => {
+	// Change the button's background color
+	banner_1b.style.display = "grid";
+});
+
+// Add a mouseout event listener
+banner_1.addEventListener("mouseout", () => {
+	// Change the button's background color back to its original color
+	banner_1b.style.display = "none";
+});
+
+// Add a mouseover event listener
+banner_1b.addEventListener("mouseover", () => {
+	// Change the button's background color
+	banner_1b.style.display = "grid";
+	banner_1.style.backgroundColor = "#67809f";
+	banner_1span.style.color = "#fff";
+});
+
+// Add a mouseout event listener
+banner_1b.addEventListener("mouseout", () => {
+	// Change the button's background color back to its original color
+	banner_1b.style.display = "none";
+	banner_1.style.backgroundColor = "";
+	banner_1span.style.color = "";
+});
+
+const banner_2 = document.getElementById("banner-2");
+const banner_2b = document.getElementById("banner-2b");
+const banner_2span = document.getElementById("icon-banner-2");
+
+// Add a mouseover event listener
+banner_2.addEventListener("mouseover", () => {
+	// Change the button's background color
+	banner_2b.style.display = "grid";
+});
+
+// Add a mouseout event listener
+banner_2.addEventListener("mouseout", () => {
+	// Change the button's background color back to its original color
+	banner_2b.style.display = "none";
+});
+
+// Add a mouseover event listener
+banner_2b.addEventListener("mouseover", () => {
+	// Change the button's background color
+	banner_2b.style.display = "grid";
+	banner_2.style.backgroundColor = "#4183d7";
+	banner_2span.style.color = "#fff";
+});
+
+// Add a mouseout event listener
+banner_2b.addEventListener("mouseout", () => {
+	// Change the button's background color back to its original color
+	banner_2b.style.display = "none";
+	banner_2.style.backgroundColor = "";
+	banner_2span.style.color = "";
+});
+
+const banner_3 = document.getElementById("banner-3");
+const banner_3b = document.getElementById("banner-3b");
+const banner_3span = document.getElementById("icon-banner-3");
+
+// Add a mouseover event listener
+banner_3.addEventListener("mouseover", () => {
+	// Change the button's background color
+	banner_3b.style.display = "grid";
+});
+
+// Add a mouseout event listener
+banner_3.addEventListener("mouseout", () => {
+	// Change the button's background color back to its original color
+	banner_3b.style.display = "none";
+});
+
+// Add a mouseover event listener
+banner_3b.addEventListener("mouseover", () => {
+	// Change the button's background color
+	banner_3b.style.display = "grid";
+	banner_3.style.backgroundColor = "#2ecc71";
+	banner_3span.style.color = "#fff";
+});
+
+// Add a mouseout event listener
+banner_3b.addEventListener("mouseout", () => {
+	// Change the button's background color back to its original color
+	banner_3b.style.display = "none";
+	banner_3.style.backgroundColor = "";
+	banner_3span.style.color = "";
+});
+
+const banner_4 = document.getElementById("banner-4");
+const banner_4b = document.getElementById("banner-4b");
+const banner_4span = document.getElementById("icon-banner-4");
+
+// Add a mouseover event listener
+banner_4.addEventListener("mouseover", () => {
+	// Change the button's background color
+	banner_4b.style.display = "grid";
+});
+
+// Add a mouseout event listener
+banner_4.addEventListener("mouseout", () => {
+	// Change the button's background color back to its original color
+	banner_4b.style.display = "none";
+});
+
+// Add a mouseover event listener
+banner_4b.addEventListener("mouseover", () => {
+	// Change the button's background color
+	banner_4b.style.display = "grid";
+	banner_4.style.backgroundColor = "#d64541";
+	banner_4span.style.color = "#fff";
+});
+
+// Add a mouseout event listener
+banner_4b.addEventListener("mouseout", () => {
+	// Change the button's background color back to its original color
+	banner_4b.style.display = "none";
+	banner_4.style.backgroundColor = "";
+	banner_4span.style.color = "";
+});
+
+const banner_5 = document.getElementById("banner-5");
+const banner_5b = document.getElementById("banner-5b");
+const banner_5span = document.getElementById("icon-banner-5");
+
+// Add a mouseover event listener
+banner_5.addEventListener("mouseover", () => {
+	// Change the button's background color
+	banner_5b.style.display = "grid";
+});
+
+// Add a mouseout event listener
+banner_5.addEventListener("mouseout", () => {
+	// Change the button's background color back to its original color
+	banner_5b.style.display = "none";
+});
+
+// Add a mouseover event listener
+banner_5b.addEventListener("mouseover", () => {
+	// Change the button's background color
+	banner_5b.style.display = "grid";
+	banner_5.style.backgroundColor = "#926fb1";
+	banner_5span.style.color = "#fff";
+});
+
+// Add a mouseout event listener
+banner_5b.addEventListener("mouseout", () => {
+	// Change the button's background color back to its original color
+	banner_5b.style.display = "none";
+	banner_5.style.backgroundColor = "";
+	banner_5span.style.color = "";
+});
+
+const banner_6 = document.getElementById("banner-6");
+const banner_6b = document.getElementById("banner-6b");
+const banner_6span = document.getElementById("icon-banner-6");
+
+// Add a mouseover event listener
+banner_6.addEventListener("mouseover", () => {
+	// Change the button's background color
+	banner_6b.style.display = "grid";
+});
+
+// Add a mouseout event listener
+banner_6.addEventListener("mouseout", () => {
+	// Change the button's background color back to its original color
+	banner_6b.style.display = "none";
+});
+
+// Add a mouseover event listener
+banner_6b.addEventListener("mouseover", () => {
+	// Change the button's background color
+	banner_6b.style.display = "grid";
+	banner_6.style.backgroundColor = "#f62459";
+	banner_6span.style.color = "#fff";
+});
+
+// Add a mouseout event listener
+banner_6b.addEventListener("mouseout", () => {
+	// Change the button's background color back to its original color
+	banner_6b.style.display = "none";
+	banner_6.style.backgroundColor = "";
+	banner_6span.style.color = "";
+});
+
+const banner_7 = document.getElementById("banner-7");
+const banner_7b = document.getElementById("banner-7b");
+const banner_7span = document.getElementById("icon-banner-7");
+
+// Add a mouseover event listener
+banner_7.addEventListener("mouseover", () => {
+	// Change the button's background color
+	banner_7b.style.display = "grid";
+});
+
+// Add a mouseout event listener
+banner_7.addEventListener("mouseout", () => {
+	// Change the button's background color back to its original color
+	banner_7b.style.display = "none";
+});
+
+// Add a mouseover event listener
+banner_7b.addEventListener("mouseover", () => {
+	// Change the button's background color
+	banner_7b.style.display = "grid";
+	banner_7.style.backgroundColor = "#67809f";
+	banner_7span.style.color = "#fff";
+});
+
+// Add a mouseout event listener
+banner_7b.addEventListener("mouseout", () => {
+	// Change the button's background color back to its original color
+	banner_7b.style.display = "none";
+	banner_7.style.backgroundColor = "";
+	banner_7span.style.color = "";
+});
