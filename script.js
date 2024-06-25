6dbe1d40cd9c70f397515ab6d4cf74fadfab867e
DominicS97 <dejfsutcliffe@gmail.com>
2024-06-25 15:09:09 +0100

    added slider

--- script.js
+++ script.js
@@ -324,3 +324,23 @@ window.onscroll = function () {
 
 	lastsscrollPos = scrollPos;
 };
+
+//
+// IMAGE SLIDER
+//
+
+let currentSlide = 1;
+changeSlide(1);
+
+function changeSlide(n) {
+	let slides = document.getElementsByClassName("slider-item");
+	let dots = document.getElementsByClassName("dot");
+	for (let i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	for (let i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" active", "");
+	}
+	slides[n - 1].style.display = "block";
+	dots[n - 1].className += " active";
+}
