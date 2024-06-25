eaf184caaa8457d1fa2539a3a9771b57f62fe9ef
DominicS97 <dejfsutcliffe@gmail.com>
2024-06-25 21:31:57 +0100

    uncommitted changes

--- script.js
+++ script.js
@@ -341,6 +341,18 @@ function changeSlide(n) {
 	for (let i = 0; i < dots.length; i++) {
 		dots[i].className = dots[i].className.replace(" active", "");
 	}
-	slides[n - 1].style.display = "block";
+	currentSlide = n;
+	slides[n - 1].style.display = "flex";
 	dots[n - 1].className += " active";
 }
+
+function nextSlide() {
+	let m = currentSlide;
+	if ((m = 7)) {
+		m = 1;
+	} else {
+		m++;
+	}
+	changeSlide(m);
+}
+setInterval(nextSlide(), 1000);
